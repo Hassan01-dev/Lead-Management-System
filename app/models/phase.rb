4b642fd5582b7bf2c45f7f7895652d77f0020766
Hassan Raza <88370398+Hassan-Devsinc@users.noreply.github.com>
2021-09-13 13:47:12 +0500

    Revert "Basic UI for Lead"

--- app/models/phase.rb
+++ app/models/phase.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Phase < ApplicationRecord
-  # serialize :assigned_engineer, Array
   belongs_to :user
   belongs_to :lead
   has_and_belongs_to_many :users # rubocop:disable Rails/HasAndBelongsToMany
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
