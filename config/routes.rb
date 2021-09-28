a0237f9123346e317db3e9341304c6a2a3a42813
Hassan-Devsinc <hassan.raza@devsinc.com>
2021-09-28 17:41:43 +0500

    Minor Changes in code

--- config/routes.rb
+++ config/routes.rb
@@ -16,14 +16,14 @@ Rails.application.routes.draw do
 
   post '/comments/:id/delete_image/', to: 'comments#delete_image', as: 'delete_image'
   get '/users/:id/change_password', to: 'users#password_change', as: 'password_change'
-  patch '/users/:id', to: 'users#password_update'
+  patch '/password_update/:id', to: 'users#password_update', as: 'password_update'
   # root to: 'dashboard#login'
   root to: redirect('/users/sign_in')
   devise_for :users
 
   resources :users
 
-  get '*path', to: redirect('/'), code: 404
+  get "*path", to: redirect('/'), code: 404
   # match '*path', to: redirect("#{Rails.root}/public/404"), via: :get, code: 404
   # get '*unmatched_route', to: 'leads#index', code: 404
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
